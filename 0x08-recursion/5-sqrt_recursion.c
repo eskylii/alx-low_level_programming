b41b68831d2f4c01240337985282176d49616cc1
eskylii <essaidisouad20@gmail.com>
2023-09-28 17:09:59 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (new_sqrt_recursion(n, 0));
 }
 
 /**
@@ -26,6 +26,6 @@ int new_sqrt_recursion(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (new_sqrt_recursion(n, i + 1));
 }
 
